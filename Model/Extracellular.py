Renaming gradient class to extracellular and fixed functionality with the new extracellular class. Input now relies on setting line by line instead of every three to add flexibility

Model/Extracellular.py
@@ -3,16 +3,17 @@ import random as r
 
 
 
-class Gradient:
+class Extracellular:
     """ Initialization called once. Class holds information about each gradient for each simulation
     """
-    def __init__(self, size, dx, dy, dz, diffuse_const, avg_initial):
+    def __init__(self, size, dx, dy, dz, diffuse_const, avg_initial, parallel):
         """ size: dimensions of the environment space
             dx: x direction step size
             dy: y direction step size
             dz: z direction step size
             diffuse_const: diffusion constant
             avg_initial: the average initial value for each concentration point
+            parallel: whether gpu processing is being used
         """
         self.size = size
         self.dx = dx
@@ -20,6 +21,7 @@ class Gradient:
         self.dz = dz
         self.diffuse_const = diffuse_const
         self.avg_initial = avg_initial
+        self.parallel = parallel
 
         # squaring the approximation of the differential
         self.dx2 = dx ** 2
@@ -30,20 +32,32 @@ class Gradient:
         self.dt = (self.dx2 * self.dy2 * self.dz2) / (2 * diffuse_const * (self.dx2 + self.dy2 + self.dz2))
 
         # the points at which the diffusion values are calculated
-        self.diffuse_values = np.zeros((size[0] // dx, size[1] // dy, size[2] // dz))
+        x_size = int(self.size[0] / dx)
+        y_size = int(self.size[1] / dy)
+        z_size = int(self.size[2] / dz)
+        self.diffuse_values = np.zeros((x_size, y_size, z_size))
 
 
-    def initialize_diffusion(self):
+    def initialize(self):
         """ Set up the environment space with a series
             of concentration values
         """
-        for i in range(len(self.diffuse_values[0])):
-            for j in range(len(self.diffuse_values[1])):
-                for k in range(len(self.diffuse_values[2])):
-                    self.diffuse_values = r.random() * self.avg_initial
+        # if the simulation is in 2D
+
+        if self.size[2] == 0:
+            for i in range(len(self.diffuse_values.size[0])):
+                for j in range(len(self.diffuse_values.size[1])):
+                    self.diffuse_values[i][j][0] = r.random() * self.avg_initial
+
+        # if the simulation is in 3D
+        else:
+            for i in range(len(self.diffuse_values.size[0])):
+                for j in range(len(self.diffuse_values.size[1])):
+                    for k in range(len(self.diffuse_values.size[2])):
+                        self.diffuse_values[i][j][k] = r.random() * self.avg_initial
 
 
-    def update_diffusion(self, simulation):
+    def update(self, simulation):
         """ Updates the environment space by "smoothing"
             the concentrations of the space
         """

Model/Input.py
@@ -4,7 +4,7 @@ import os
 import shutil
 import platform
 
-from Model import Gradient
+from Model import Extracellular
 from Model import Simulation
 from Model import Cell
 
@@ -35,43 +35,43 @@ def Setup():
 
     # loops over all of the files in the director and turns the file lines into a list
     for file in files:
-        setup_file = open(input_path + separator + file, "r")
-        setup_list = setup_file.readlines()
-        parameters = []
+        parameters_file = open(input_path + separator + file, "r")
+        parameters = parameters_file.readlines()
 
-        # looks at every third line, the ones with parameters
-        for i in range(1, len(setup_list), 3):
-            parameters.append(setup_list[i][2:-3])
-
-        # organizes all of the parameters and converts them from strings to their desired type
-        _path = check_name(output_path, parameters[0], separator)
-        _parallel = eval(parameters[1])
-        _end_time = float(parameters[2])
-        _time_step = float(parameters[3])
-        _num_GATA6 = int(parameters[4])
-        _num_NANOG = int(parameters[5])
-        _stochastic = bool(parameters[6])
-        _size = eval(parameters[7])
-        _functions = eval(parameters[8])
-        _pluri_div_thresh = float(parameters[9])
-        _diff_div_thresh = float(parameters[10])
-        _pluri_to_diff = float(parameters[11])
-        _diff_surround_value = int(parameters[12])
-        _death_threshold = float(parameters[13])
-        _move_time_step = float(parameters[14])
-        _move_max_time = float(parameters[15])
-        _spring_constant = float(parameters[16])
-        _friction = float(parameters[17])
-        _energy_kept = float(parameters[18])
-        _neighbor_distance = float(parameters[19])
-        _mass = float(parameters[20])
-        _gradients = eval(parameters[21])
-        _density = float(parameters[22])
-        _num_states = int(parameters[23])
-        _quality = int(parameters[24])
-        _group = int(parameters[25])
-        _speed = int(parameters[26])
-        _max_radius = float(parameters[27])
+        # looks at certain lines of the template file and converts them from strings to their desired type
+        _path = check_name(output_path, parameters[1][2:-3], separator)
+        _parallel = eval(parameters[4][2:-3])
+        _end_time = float(parameters[7][2:-3])
+        _time_step = float(parameters[10][2:-3])
+        _num_GATA6 = int(parameters[13][2:-3])
+        _num_NANOG = int(parameters[16][2:-3])
+        _stochastic = bool(parameters[19][2:-3])
+        _size = eval(parameters[22][2:-3])
+        _functions = eval(parameters[25][2:-3])
+        _pluri_div_thresh = float(parameters[28][2:-3])
+        _diff_div_thresh = float(parameters[31][2:-3])
+        _pluri_to_diff = float(parameters[34][2:-3])
+        _diff_surround_value = int(parameters[37][2:-3])
+        _death_threshold = float(parameters[40][2:-3])
+        _move_time_step = float(parameters[43][2:-3])
+        _move_max_time = float(parameters[46][2:-3])
+        _spring_constant = float(parameters[49][2:-3])
+        _friction = float(parameters[52][2:-3])
+        _energy_kept = float(parameters[55][2:-3])
+        _neighbor_distance = float(parameters[58][2:-3])
+        _mass = float(parameters[61][2:-3])
+        _gradients = eval(parameters[64][2:-3])
+        _density = float(parameters[67][2:-3])
+        _num_states = int(parameters[70][2:-3])
+        _quality = int(parameters[73][2:-3])
+        _group = int(parameters[76][2:-3])
+        _speed = int(parameters[79][2:-3])
+        _max_radius = float(parameters[82][2:-3])
+        _dx = float(parameters[85][2:-3])
+        _dy = float(parameters[88][2:-3])
+        _dz = float(parameters[91][2:-3])
+        _diffuse_const = float(parameters[94][2:-3])
+        _avg_initial = float(parameters[97][2:-3])
 
         # initializes simulation class which holds all information about the simulation
         simulation = Simulation.Simulation(_path, _end_time, _time_step, _pluri_div_thresh, _diff_div_thresh,
@@ -83,15 +83,14 @@ def Setup():
         # copies the setup file to the new directory of the simulation
         shutil.copy(input_path + separator + file, simulation.path)
 
-
         # loops over the gradients and adds them to the simulation
         for i in range(len(_gradients)):
 
-            # initializes the gradient class
-            new_gradient = Gradient.Gradient(_gradients[i][0], _size, int(_gradients[i][1]), _parallel)
+            # initializes the extracellular class
+            new_extracellular = Extracellular.Extracellular(_size, _dx, _dy, _dz, _diffuse_const, _avg_initial, _parallel)
 
-            # adds the gradient object
-            simulation.gradients = np.append(simulation.gradients, new_gradient)
+            # adds the Extracellular object
+            simulation.extracellular = np.append(simulation.extracellular, new_extracellular)
 
 
         # loops over all cells and creates a stem cell object for each one with given parameters

Model/Simulation.py
@@ -72,8 +72,8 @@ class Simulation:
         # array to hold all of the cell objects
         self.cells = np.array([], dtype=np.object)
 
-        # array to hold all of the gradient objects
-        self.gradients = np.array([], dtype=np.object)
+        # array to hold all of the Extracellular objects
+        self.extracellular = np.array([], dtype=np.object)
 
         # graph representing all the cells and their connections to other cells
         self.network = nx.Graph()
@@ -90,17 +90,17 @@ class Simulation:
         print("Time: " + str(self.time_counter))
         print("Number of objects: " + str(len(self.cells)))
 
-    def initialize_gradients(self):
+    def initialize_diffusion(self):
         """ see Cell.py for definition
         """
-        for i in range(len(self.gradients)):
-            self.gradients[i].initialize_grid()
+        for i in range(len(self.extracellular)):
+            self.extracellular[i].initialize()
 
-    def update_gradients(self):
+    def update_diffusion(self):
         """ see Cell.py for definition
         """
-        for i in range(len(self.gradients)):
-            self.gradients[i].update_grid()
+        for i in range(len(self.extracellular)):
+            self.extracellular[i].update()
 
     def update_cells(self):
         """ see Cell.py for definition