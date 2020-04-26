1a2fe631d18e936a9c11a4543865ddf3f14a96ee
JackToppen <57497258+JackToppen@users.noreply.github.com>
2020-04-26 17:05:46 -0500

    Introduced image slicing. Divides the space into sections in the z direction and then adjusts the radius based on the section size

--- Model/Output.py
+++ Model/Output.py
@@ -10,73 +10,92 @@ def draw_image(simulation):
     # increases the image counter by 1 each time this is called
     simulation.image_counter += 1
 
+    # thickness of the image slice in the z direction
+    thickness = simulation.size[2] / simulation.slices
+
     # get the dilation of the image for the correctly sizing the image
     dilation_x = simulation.quality[0] / simulation.size[0]
     dilation_y = simulation.quality[1] / simulation.size[1]
 
-    # draws the background of the image
-    base = Image.new("RGB", simulation.quality[0:2], color=(255, 255, 255))
-    image = ImageDraw.Draw(base)
-
     # bounds of the simulation used for drawing lines
-    bounds = np.array([[0, 0],[0, simulation.size[1]],[simulation.size[0],simulation.size[1]],[simulation.size[0], 0]])
+    corner_1 = [0, 0]
+    corner_2 = [0, simulation.size[1]]
+    corner_3 = [simulation.size[0], simulation.size[1]]
+    corner_4 = [simulation.size[0], 0]
+    bounds = np.array([corner_1, corner_2, corner_3, corner_4])
 
-    # loops over all of the cells and draws the nucleus and radius
-    for i in range(len(simulation.cells)):
-        # get location in 2D
-        x = dilation_x * simulation.cells[i].location[0]
-        y = dilation_y * simulation.cells[i].location[1]
+    # starting z value for slice location
+    slice_level = 0
 
-        # gets membrane sizing
-        membrane_x = dilation_x * simulation.cells[i].radius
-        membrane_y = dilation_y * simulation.cells[i].radius
+    for i in range(simulation.slices):
 
-        # coloring of the cells
-        if simulation.cells[i].state == "Pluripotent":
-            color = 'green'
-        else:
-            color = 'red'
+        # draws the background of the image
+        base = Image.new("RGB", simulation.quality[0:2], color=(255, 255, 255))
+        image = ImageDraw.Draw(base)
 
-        # draw the circle representing the cell
-        membrane_circle = (x - membrane_x, y - membrane_y, x + membrane_x, y + membrane_y)
-        image.ellipse(membrane_circle, outline="black", fill=color)
+        # loops over all of the bounds and draws lines to represent the grid
+        for j in range(len(bounds)):
+            # get the bound sizing
+            x0 = dilation_x * bounds[j][0]
+            y0 = dilation_y * bounds[j][1]
 
-    # loops over all of the bounds and draws lines to represent the grid
-    for i in range(len(bounds)):
-        # get the bound sizing
-        x0 = dilation_x * bounds[i][0]
-        y0 = dilation_y * bounds[i][1]
+            # get the bounds as lines
+            if j < len(bounds) - 1:
+                x1 = dilation_x * bounds[j + 1][0]
+                y1 = dilation_y * bounds[j + 1][1]
+            else:
+                x1 = dilation_x * bounds[0][0]
+                y1 = dilation_y * bounds[0][1]
 
-        # get the bounds as lines
-        if i < len(bounds) - 1:
-            x1 = dilation_x * bounds[i + 1][0]
-            y1 = dilation_y * bounds[i + 1][1]
-        else:
-            x1 = dilation_x * bounds[0][0]
-            y1 = dilation_y * bounds[0][1]
+            # draw the lines
+            lines = (x0, y0, x1, y1)
+            color = 'black'
+            image.line(lines, fill=color, width=10)
 
-        # draw the lines
-        lines = (x0, y0, x1, y1)
-        color = 'black'
-        image.line(lines, fill=color, width=10)
+        # loops over all of the cells and draws the nucleus and radius
+        for j in range(len(simulation.cells)):
+            location = simulation.cells[j].location
+            radius = simulation.cells[j].radius
 
-    # saves the image as a .png
-    base.save(simulation.path + "network_image_" + str(int(simulation.time_counter)) + ".png", 'PNG')
+
+            x_radius = dilation_x * max(radius ** 2 - (location[2] - slice_level) ** 2, 0.0) ** 0.5
+            y_radius = dilation_y * max(radius ** 2 - (location[2] - slice_level) ** 2, 0.0) ** 0.5
+
+            # get location in 2D with image size dilation
+            x = dilation_x * location[0]
+            y = dilation_y * location[1]
+
+            # coloring of the cells
+            if simulation.cells[j].state == "Pluripotent":
+                color = 'green'
+            else:
+                color = 'red'
+
+            # draw the circle representing the cell
+            membrane_circle = (x - x_radius, y - y_radius, x + x_radius, y + y_radius)
+            image.ellipse(membrane_circle, outline="black", fill=color)
+
+        # saves the image as a .png
+        image_name = "image_" + str(int(simulation.time_counter)) + "_slice_" + str(int(i)) + ".png"
+        base.save(simulation.path + image_name, 'PNG')
+
+        # moves to the next slice location
+        slice_level += thickness
+
 
 def image_to_video(simulation):
     """ Creates a video out of all the png images at
         the end of the simulation
     """
     image_quality = simulation.quality
-    image_size = (image_quality * 1500, image_quality * 1500)
 
     # creates a base video file to save to
     video_path = simulation.path + 'network_video.avi'
-    out = cv2.VideoWriter(video_path, cv2.VideoWriter_fourcc("M","J","P","G"), 1.0, image_size)
+    out = cv2.VideoWriter(video_path, cv2.VideoWriter_fourcc("M","J","P","G"), 1.0, image_quality)
 
     # loops over all images and writes them to the base video file
     for i in range(simulation.image_counter):
-        path = simulation.path + 'network_image_' + str(i) + ".png"
+        path = simulation.path + "image_" + str(i) + "_slice_0" + ".png"
         image = cv2.imread(path)
         out.write(image)
 
