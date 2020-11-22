472d1f77ee838fe002dd8033d253618d10e972e9
JackToppen <JaToppen3748@gmail.com>
2020-11-22 14:11:39 -0600

    Commenting to run.py

--- Model/run.py
+++ Model/run.py
@@ -10,8 +10,8 @@ simulation = input.setup()
 
 # Define the names of any cell types. These names will be used to begin the model with a set number of cells that
 # correspond to the particular initial parameters for that cell type.
-simulation.cell_type("NANOG_high", 1000)
-simulation.cell_type("GATA6_high", 0)
+simulation.cell_type("NANOG_high", simulation.num_nanog)
+simulation.cell_type("GATA6_high", simulation.num_gata6)
 
 # Define the cell arrays used to store values of the cell. Each tuple corresponds to a cell array that will be
 # generated. The first index of the tuple is the instance variable name for the Simulation class, the second being the
@@ -45,58 +45,56 @@ simulation.initials("rotation", lambda: r.random() * 360)
 simulation.initials("GATA6", lambda: r.randrange(0, simulation.field), cell_type="GATA6_high")
 simulation.initials("NANOG", lambda: 0, cell_type="GATA6_high")
 
-# places all of the diffusion points into bins so that the model can use a bin sorting method to when determining
-# highest/lowest concentrations of the extracellular gradient(s)
+# Places all of the diffusion points into bins so that the model can use a bin sorting method when determining
+# cell motility based on diffusion point locations. (not in use)
 # functions.setup_diffusion_bins(simulation)
 
-# this will loop over all steps defined in the general template file in addition to updating the current step
-# of the simulation. this is done to explicitly/easily show what happens at each step.
+# Add any functions under the loop that will be called during each step of the simulation.
 for simulation.current_step in range(simulation.beginning_step, simulation.end_step + 1):
-    # prints the current step, number of cells, and records run time. used to give an idea of the simulation progress.
+    # Prints the current step, number of cells, and records model run time.
     functions.info(simulation)
 
-    # updates the graph used to hold neighbors of cells within a fixed radius.
+    # Find the neighbors of each cell that are within a fixed radius and stores this info in a graph.
     functions.check_neighbors(simulation)
 
-    # updates cells by adjusting trackers for differentiation and division based on intracellular, intercellular,
-    # and extracellular conditions. this is done through a series of methods
+    # Updates cells by adjusting trackers for differentiation and division based on intracellular, intercellular,
+    # and extracellular conditions through a series of methods.
     functions.cell_death(simulation)
     functions.cell_diff_surround(simulation)
     functions.cell_growth(simulation)
     functions.cell_division(simulation)
     functions.cell_pathway(simulation)
 
-    # updates each of the extracellular gradients via the finite difference method after cells have interacted
-    # with the gradient in cell_update().
-    # functions.update_diffusion(simulation)
+    # Simulates the diffusion for each of the extracellular gradients via the forward time centered space method.
+    functions.update_diffusion(simulation)
 
-    # adds/removes cells to/from the simulation either all together or in desired groups of cells. if done in
+    # Adds/removes cells to/from the simulation either all together or in desired groups of cells. If done in
     # groups, the handle_movement() function will be used to better represent asynchronous division and death.
     functions.update_queue(simulation)
 
-    # find the nearest NANOG high, GATA6 high, and differentiated cell within a fixed radius. this provides information
-    # that can potentially be used for movement such as moving toward the nearest NANOG high cell
+    # Find the nearest NANOG high, GATA6 high, and differentiated cells within a fixed radius. this provides
+    # information that can potentially be used for approximating cell motility. (not in use)
     # functions.nearest(simulation)
 
-    # find the nearest pluripotent cell within a fixed radius that is not part of the same component of the underlying
-    # graph of all pluripotent cells. used to represent the movement of pluripotent clusters
+    # Find the nearest pluripotent cell within a fixed radius that is not part of the same component of the underlying
+    # graph of all pluripotent cells. Used to represent the movement of pluripotent clusters. (not in use)
     # functions.nearest_cluster(simulation)
 
-    # locate the diffusion point (within a fixed radius) that has the highest FGF4 concentration. can be used to
-    # represent chemotactic movement of cells
+    # Locate the diffusion point (within a fixed radius) that has the highest FGF4 concentration. Can be used to
+    # approximate the chemotactic movement of cells. (not in use)
     # functions.highest_fgf4(simulation)
 
-    # calculates the direction/magnitude of the movement of the cell depending on a variety of factors such as state,
-    # extracellular gradient, and presence of neighbors
+    # Calculates the direction/magnitude of the movement of the cell depending on a variety of factors such as state,
+    # extracellular gradient, and presence of neighbors.
     functions.cell_motility(simulation)
 
-    # moves the cells to a state of physical equilibrium between adhesive and repulsive forces acting on the cells,
-    # while also applying active movement forces from the previous cell_motility() function.
+    # Attempts to move the cells to a state of physical equilibrium between adhesive and repulsive forces acting on
+    # the cells, while also applying active motility forces from the previous cell_motility() function.
     functions.handle_movement(simulation)
 
-    # saves multiple forms of information about the simulation at the current step, including an image of the space,
-    # csvs with values of the cells, a temporary pickle of the Simulation instance, and performance stats.
+    # Saves multiple forms of information about the simulation at the current step, including an image of the space,
+    # CSVs with values of the cells, a temporary pickle of the Simulation instance, and performance stats.
     output.step_outputs(simulation)
 
-# ends the simulation by creating a video from all of the images created by the simulation
+# Ends the simulation by creating a video from all of the step images
 output.create_video(simulation)
