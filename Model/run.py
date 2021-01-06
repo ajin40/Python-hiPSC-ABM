c894b0452570a1bf01bb9f2c38045f4b7e2548f1
JackToppen <JaToppen3748@gmail.com>
2021-01-05 19:16:16 -0600

    Reorganized how the model is initialized from run.py

--- Model/run.py
+++ Model/run.py
@@ -5,13 +5,12 @@ import output
 import backend
 import functions
 
+# call input.start() if this file is being run directly and not imported
+if __name__ == "__main__":
+    input.start()
 
-# setup() will direct how the model is run based on inputted parameters. If a new simulation is desired, setup()
-# will return an instance of the Simulation object which holds all important information of the simulation as it runs.
-simulation = input.setup()
 
-# For continuing a previous simulation (mode 1), bypass the initialization of cell array values
-if simulation.mode == 0:
+def setup_cells(simulation):
     # Define the number of cells for each cell type. These names can be used to initialize the model with specific
     # numbers of cell types that may differ in initial parameters.
     simulation.cell_type("NANOG_high", simulation.num_nanog)
@@ -41,45 +40,47 @@ if simulation.mode == 0:
     simulation.alt_initials("GATA6", "GATA6_high", lambda: r.randrange(1, simulation.field))
     simulation.alt_initials("NANOG", "GATA6_high", lambda: 0)
 
-# Add any functions under the loop that will be called during each step of the simulation.
-for simulation.current_step in range(simulation.beginning_step, simulation.end_step + 1):
-    # Records model run time for the step and prints the current step/number of cells,
-    backend.info(simulation)
 
-    # Finds the neighbors of each cell that are within a fixed radius and store this info in a graph.
-    functions.get_neighbors(simulation)
+def steps(simulation):
+    # Add any functions under the loop that will be called during each step of the simulation.
+    for simulation.current_step in range(simulation.beginning_step, simulation.end_step + 1):
+        # Records model run time for the step and prints the current step/number of cells,
+        backend.info(simulation)
 
-    # Updates cells by adjusting trackers for differentiation, division, growth, etc. based on intracellular,
-    # intercellular, and extracellular conditions through a series of separate methods.
-    functions.cell_death(simulation)
-    functions.cell_diff_surround(simulation)
-    functions.cell_division(simulation)
-    functions.cell_growth(simulation)
-    functions.cell_pathway(simulation)
+        # Finds the neighbors of each cell that are within a fixed radius and store this info in a graph.
+        functions.get_neighbors(simulation)
 
-    # Simulates the diffusion for each of the extracellular gradients via the forward time centered space method.
-    functions.update_diffusion(simulation)
+        # Updates cells by adjusting trackers for differentiation, division, growth, etc. based on intracellular,
+        # intercellular, and extracellular conditions through a series of separate methods.
+        functions.cell_death(simulation)
+        functions.cell_diff_surround(simulation)
+        functions.cell_division(simulation)
+        functions.cell_growth(simulation)
+        functions.cell_pathway(simulation)
 
-    # Adds/removes cells to/from the simulation either all together or in desired groups of cells. If done in
-    # groups, the handle_movement() function will be used to better represent asynchronous division and death.
-    functions.update_queue(simulation)
+        # Simulates the diffusion for each of the extracellular gradients via the forward time centered space method.
+        # functions.update_diffusion(simulation)
 
-    # Finds the nearest NANOG high, GATA6 high, and differentiated cells within a fixed radius. This provides
-    # information that can be used for approximating cell motility.
-    functions.nearest(simulation)
+        # Adds/removes cells to/from the simulation either all together or in desired groups of cells. If done in
+        # groups, the handle_movement() function will be used to better represent asynchronous division and death.
+        functions.update_queue(simulation)
 
-    # Calculates the direction/magnitude of a cell's movement depending on a variety of factors such as state
-    # and presence of neighbors.
-    functions.cell_motility(simulation)
-    # functions.eunbi_motility(simulation)
+        # Finds the nearest NANOG high, GATA6 high, and differentiated cells within a fixed radius. This provides
+        # information that can be used for approximating cell motility.
+        functions.nearest(simulation)
 
-    # Attempts to move the cells to a state of physical equilibrium between adhesive and repulsive forces acting on
-    # the cells, while applying active motility forces from the previous cell_motility() function.
-    functions.handle_movement(simulation)
+        # Calculates the direction/magnitude of a cell's movement depending on a variety of factors such as state
+        # and presence of neighbors.
+        functions.cell_motility(simulation)
+        # functions.eunbi_motility(simulation)
 
-    # Saves multiple forms of information about the simulation at the current step, including an image of the space,
-    # CSVs with values of the cells, a temporary pickle of the Simulation object, and performance stats.
-    output.step_outputs(simulation)
+        # Attempts to move the cells to a state of physical equilibrium between adhesive and repulsive forces acting on
+        # the cells, while applying active motility forces from the previous cell_motility() function.
+        functions.handle_movement(simulation)
 
-# Ends the simulation by creating a video from all of the step images
-output.create_video(simulation)
+        # Saves multiple forms of information about the simulation at the current step, including an image of the space,
+        # CSVs with values of the cells, a temporary pickle of the Simulation object, and performance stats.
+        output.step_outputs(simulation)
+
+    # Ends the simulation by creating a video from all of the step images
+    output.create_video(simulation)
